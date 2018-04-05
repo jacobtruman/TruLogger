f1abf1903427d4398dd909a2b258d92083298f9f
Jacob Truman <jatruman@adobe.com>
2018-04-04 18:01:10 -0700

    Initial commit

---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest
+import trulogger
+
+
+logger = trulogger.TruLogger({'verbose': True})
+
+logger.set_prefix("custom prefix")
+
+logger.info("This is info")
+logger.error("This is error")
+logger.warning("This is warning")
+logger.debug("This is debug")
+logger.success("This is an success")
+
+logger.add_to_log("This is a simple add")
